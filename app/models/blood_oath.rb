0f9fa50da1a0c6a9a1fc5ddf72ab20f4310ccfb3
frod25 <frod25>
2020-05-19 11:16:30 -0400

    all deliverables except last method in blood_oath.rb (bonus methods not complete yet)

---
+++ app/models/blood_oath.rb
@@ -0,0 +1,17 @@
+class BloodOath
+    attr_accessor :follower, :cult, :initiation_date
+
+    @@all = []
+
+    def initialize(follower, cult, initiation_date)
+        @follower = follower
+        @cult = cult
+        @initiation_date = initiation_date
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+cult1 = Cult.new("name1", "location1", 2005, "slogan1")
+cult2 = Cult.new("name2", "location2", 2005, "slogan2")
+cult3 = Cult.new("name3", "location1", 2004, "slogan3")
+
+follower1 = Follower.new("follower1", 30, "life_motto1")
+follower2 = Follower.new("follower2", 31, "life_motto2")
+follower3 = Follower.new("follower3", 32, "life_motto3")
+
+# (follower1, cult1, "2005-02-05")
+bo2 = BloodOath.new(follower1, cult2, "2005-03-06")
+bo3 = BloodOath.new(follower3, cult1, "2005-02-05")
+
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
